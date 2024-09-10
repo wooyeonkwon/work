7e3e82d1f57d1ee21205898e59da0e26bc3b6bef
wooyeon kwon <wkwon@cern.ch>
2024-09-10 03:54:06 +0200

    not rpc has been removed

--- Analysis/drawHistograms_cbf.C
+++ Analysis/drawHistograms_cbf.C
@@ -5,6 +5,7 @@
 #include "TH1F.h"
 #include "TF1.h"
 #include "TCanvas.h"
+#include "TLatex.h"
 
 double dscbf(double *x, double *par)
 { 
@@ -38,8 +39,6 @@ double dscbf(double *x, double *par)
     return N*result;
 }
 
-
-
 void drawHistograms_cbf(const char* filename) {
     TFile *file = new TFile(filename, "READ");
     if (!file || file->IsZombie()) {
@@ -50,12 +49,10 @@ void drawHistograms_cbf(const char* filename) {
 
     TTree *treeGlobal = nullptr;
     TTree *treeRPC = nullptr;
-    TTree *treenotRPC = nullptr;
     file->GetObject("GlobalMuons", treeGlobal);
     file->GetObject("RPCMuons", treeRPC);
-    file->GetObject("notRPCMuons", treenotRPC);
 
-    if (!treeGlobal || !treeRPC || !treenotRPC) {
+    if (!treeGlobal || !treeRPC) {
         std::cerr << "Error: Trees not found in file" << std::endl;
         file->Close();
         delete file;
@@ -66,40 +63,31 @@ void drawHistograms_cbf(const char* filename) {
 
     double zBosonMassGlobal;
     double zBosonMassRPC;
-    double zBosonMassnotRPC;
 
     treeGlobal->SetBranchAddress("zBosonMass", &zBosonMassGlobal);
     treeRPC->SetBranchAddress("zBosonMass", &zBosonMassRPC);
-    treenotRPC->SetBranchAddress("zBosonMass", &zBosonMassnotRPC);
 
     TH1F *h_zBosonsGlobal = new TH1F("zBosonsGlobal", "Z Bosons (Global);Mass (GeV);Events", 360, 0, 180);
     TH1F *h_zBosonsRPC = new TH1F("zBosonsRPC", "Z Bosons (RPC);Mass (GeV);Events", 360, 0, 180);
-    TH1F *h_zBosonsnotRPC = new TH1F("zBosonsnotRPC", "Z Bosons (notRPC);Mass (GeV);Events", 360, 0, 180);
     
     Long64_t nEntriesGlobal = treeGlobal->GetEntries();
     for (Long64_t i = 0; i < nEntriesGlobal; ++i) {
         treeGlobal->GetEntry(i);
         h_zBosonsGlobal->Fill(zBosonMassGlobal);
     }
-
+  
     Long64_t nEntriesRPC = treeRPC->GetEntries();
     for (Long64_t i = 0; i < nEntriesRPC; ++i) {
         treeRPC->GetEntry(i);
         h_zBosonsRPC->Fill(zBosonMassRPC);
     }
     
-    Long64_t nEntriesnotRPC = treenotRPC->GetEntries();
-    for (Long64_t i = 0; i < nEntriesnotRPC; ++i) {
-        treenotRPC->GetEntry(i);
-        h_zBosonsnotRPC->Fill(zBosonMassnotRPC);
-    }
 
     std::ofstream logFile("fit_results.txt");
     if (!logFile.is_open()) {
         std::cerr << "Unable to open log file for writing fit results." << std::endl;
         delete h_zBosonsGlobal;
         delete h_zBosonsRPC;
-        delete h_zBosonsnotRPC;
         file->Close();
         delete file;
         return;
@@ -107,12 +95,22 @@ void drawHistograms_cbf(const char* filename) {
 
     // Fit and plot for Global Muons
     TCanvas *c1 = new TCanvas("c1", "Global Z mass", 800, 600);
+    gStyle->SetOptStat(0);
     h_zBosonsGlobal->Draw();
-    TF1 *fitFuncGlobal = new TF1("fitFuncGlobal", dscbf, 10, 170, 7);
+    TF1 *fitFuncGlobal = new TF1("fitFuncGlobal", dscbf, 60, 120, 7);
     int NeventsGlobal = h_zBosonsGlobal->GetEntries();
     fitFuncGlobal->SetParNames("AlphaL", "AlphaH", "nL", "nH", "Mean", "Sigma", "Events");
-    fitFuncGlobal->SetParameters(1.25, 1.38, 1.33, 2.02, 91, 2.1, NeventsGlobal);
+    fitFuncGlobal->SetParameters(1.25, 1.44, 1.59, 1.8, 91.2, 2.0, NeventsGlobal);
     h_zBosonsGlobal->Fit(fitFuncGlobal, "R");
+
+    // Add text to the canvas
+    TLatex latex;
+    latex.SetNDC();
+    latex.SetTextSize(0.035);
+    latex.DrawLatex(0.70, 0.85, Form("Entries: %d", NeventsGlobal));
+    latex.DrawLatex(0.70, 0.80, Form("Mean: %.2f", fitFuncGlobal->GetParameter(4)));
+    latex.DrawLatex(0.70, 0.75, Form("Sigma: %.2f", fitFuncGlobal->GetParameter(5)));
+
     c1->SaveAs("zBosonsGlobal.png");
 
     logFile << "Global Fit Parameters:\n"
@@ -126,12 +124,19 @@ void drawHistograms_cbf(const char* filename) {
 
     // Fit and plot for RPC Muons
     TCanvas *c2 = new TCanvas("c2", "RPC Z mass", 800, 600);
+    gStyle->SetOptStat(0);
     h_zBosonsRPC->Draw();
-    TF1 *fitFuncRPC = new TF1("fitFuncRPC", dscbf, 10, 170, 7);
+    TF1 *fitFuncRPC = new TF1("fitFuncRPC", dscbf, 60, 120, 7);
     int NeventsRPC = h_zBosonsRPC->GetEntries();
     fitFuncRPC->SetParNames("AlphaL", "AlphaH", "nL", "nH", "Mean", "Sigma", "Events");
-    fitFuncRPC->SetParameters(1.25, 1.38, 1.33, 2.02, 91, 2.1, NeventsRPC);
+    fitFuncRPC->SetParameters(1.25, 1.44, 1.59, 1.8, 91.2, 2.0, NeventsRPC);
     h_zBosonsRPC->Fit(fitFuncRPC, "R");
+
+    // Add text to the canvas
+    latex.DrawLatex(0.70, 0.85, Form("Entries: %d", NeventsRPC));
+    latex.DrawLatex(0.70, 0.80, Form("Mean: %.2f", fitFuncRPC->GetParameter(4)));
+    latex.DrawLatex(0.70, 0.75, Form("Sigma: %.2f", fitFuncRPC->GetParameter(5)));
+
     c2->SaveAs("zBosonsRPC.png");
 
     logFile << "RPC Fit Parameters:\n"
@@ -143,37 +148,14 @@ void drawHistograms_cbf(const char* filename) {
             << "Sigma: " << fitFuncRPC->GetParameter(5) << "\n"
             << "Amplitude: " << fitFuncRPC->GetParameter(6) << "\n\n";
 
-    // Fit and plot for notRPC Muons
-    TCanvas *c3 = new TCanvas("c3", "notRPC Z mass", 800, 600);
-    h_zBosonsnotRPC->Draw();
-    TF1 *fitFuncnotRPC = new TF1("fitFuncnotRPC", dscbf, 10, 170, 7);
-    int NeventsnotRPC = h_zBosonsnotRPC->GetEntries();
-    fitFuncnotRPC->SetParNames("AlphaL", "AlphaH", "nL", "nH", "Mean", "Sigma", "Events");
-    fitFuncnotRPC->SetParameters(1.25, 1.38, 1.33, 2.02, 91, 2.1, NeventsnotRPC);
-    h_zBosonsnotRPC->Fit(fitFuncnotRPC, "R");
-    c3->SaveAs("zBosonsnotRPC.png");
-
-    logFile << "notRPC Fit Parameters:\n"
-            << "AlphaL: " << fitFuncnotRPC->GetParameter(0) << "\n"
-            << "AlphaH: " << fitFuncnotRPC->GetParameter(1) << "\n"
-            << "nL: " << fitFuncnotRPC->GetParameter(2) << "\n"
-            << "nH: " << fitFuncnotRPC->GetParameter(3) << "\n"
-            << "Mean: " << fitFuncnotRPC->GetParameter(4) << "\n"
-            << "Sigma: " << fitFuncnotRPC->GetParameter(5) << "\n"
-            << "Amplitude: " << fitFuncnotRPC->GetParameter(6) << "\n\n";
-
     logFile.close();
 
     delete h_zBosonsGlobal;
     delete h_zBosonsRPC;
-    delete h_zBosonsnotRPC;    
     delete fitFuncGlobal;
     delete fitFuncRPC;
-    delete fitFuncnotRPC;
     delete c1;
     delete c2;
-    delete c3;
     file->Close();
     delete file;
 }
-
