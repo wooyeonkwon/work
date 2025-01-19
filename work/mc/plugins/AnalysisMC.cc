6f4fb63db028460f5ca37edc689dbd2813714234
wkwon <wooyeonkwon@gmail.com>
2025-01-19 20:04:55 +0900

    add MCmatcher info

--- work/mc/plugins/AnalysisMC.cc
+++ work/mc/plugins/AnalysisMC.cc
@@ -48,7 +48,7 @@ private:
   float genWeight;
 
   // Z boson information - arrays for different categories
-  static const int nCategories = 7; // +1 for Gen Z
+  static const int nCategories = 11; // +1 for Gen Z
   std::array<double, nCategories> zMass;
   std::array<double, nCategories> zDvz;
 
@@ -61,19 +61,22 @@ private:
   std::vector<double> muon_vz;
 
   // Reconstructed Muon flags
-  std::vector<bool> muon_isReco;
-  std::vector<bool> muon_isTightReco;
+  std::vector<bool> muon_isGlobal;
+  std::vector<bool> muon_isTracker;
   std::vector<bool> muon_isRPC;
-  std::vector<bool> muon_isTightRPC;
   std::vector<bool> muon_isGEM;
-  std::vector<bool> muon_isTightGEM;
+  std::vector<bool> muon_isTight;
   
   // Z boson flags for reconstructed muons
   std::vector<bool> muon_isRecoZ;
-  std::vector<bool> muon_isTightRecoZ;
+  std::vector<bool> muon_isGlobalZ;
+  std::vector<bool> muon_isTightGlobalZ;
+  std::vector<bool> muon_isTrackerZ;
   std::vector<bool> muon_isRPCZ;
+  std::vector<bool> muon_isTunedRPCZ;
   std::vector<bool> muon_isTightRPCZ;
   std::vector<bool> muon_isGEMZ;
+  std::vector<bool> muon_isTunedGEMZ;
   std::vector<bool> muon_isTightGEMZ;
 
   // Generator level muon information
@@ -83,8 +86,9 @@ private:
   std::vector<double> genMuon_phi;
   std::vector<double> genMuon_vz;
   std::vector<bool> genMuon_isGenZ;
-  bool matchingFailed;
-  int nMatching;
+
+  // muon MC Matching information
+  std::vector<bool> muonMCMatchPass;
 };
 
 AnalysisMC::AnalysisMC(const edm::ParameterSet& iConfig)
@@ -109,20 +113,28 @@ void AnalysisMC::beginJob() {
 
   // Z boson information branches
   tree->Branch("zMass_Reco", &zMass[0]);
-  tree->Branch("zMass_TightReco", &zMass[1]);
-  tree->Branch("zMass_RPC", &zMass[2]);
-  tree->Branch("zMass_TightRPC", &zMass[3]);
-  tree->Branch("zMass_GEM", &zMass[4]);
-  tree->Branch("zMass_TightGEM", &zMass[5]);
-  tree->Branch("zMass_Gen", &zMass[6]);
+  tree->Branch("zMass_Global", &zMass[1]);
+  tree->Branch("zMass_TightGlobal", &zMass[2]);
+  tree->Branch("zMass_Tracker", &zMass[3]);
+  tree->Branch("zMass_RPC", &zMass[4]);
+  tree->Branch("zMass_TunedRPC", &zMass[5]);
+  tree->Branch("zMass_TightRPC", &zMass[6]);
+  tree->Branch("zMass_GEM", &zMass[7]);
+  tree->Branch("zMass_TunedGEM", &zMass[8]);
+  tree->Branch("zMass_TightGEM", &zMass[9]);
+  tree->Branch("zMass_Gen", &zMass[10]);
 
   tree->Branch("zDvz_Reco", &zDvz[0]);
-  tree->Branch("zDvz_TightReco", &zDvz[1]);
-  tree->Branch("zDvz_RPC", &zDvz[2]);
-  tree->Branch("zDvz_TightRPC", &zDvz[3]);
-  tree->Branch("zDvz_GEM", &zDvz[4]);
-  tree->Branch("zDvz_TightGEM", &zDvz[5]);
-  tree->Branch("zDvz_Gen", &zDvz[6]);
+  tree->Branch("zDvz_Global", &zDvz[1]);
+  tree->Branch("zDvz_TightGlobal", &zDvz[2]);
+  tree->Branch("zDvz_Tracker", &zDvz[3]);
+  tree->Branch("zDvz_RPC", &zDvz[4]);
+  tree->Branch("zDvz_TunedRPC", &zDvz[5]);
+  tree->Branch("zDvz_TightRPC", &zDvz[6]);
+  tree->Branch("zDvz_GEM", &zDvz[7]);
+  tree->Branch("zDvz_TunedGEM", &zDvz[8]);
+  tree->Branch("zDvz_TightGEM", &zDvz[9]);
+  tree->Branch("zDvz_Gen", &zDvz[10]);
 
   // Reconstructed muon branches
   tree->Branch("muon_charge", &muon_charge);
@@ -132,18 +144,21 @@ void AnalysisMC::beginJob() {
   tree->Branch("muon_iso", &muon_iso);
   tree->Branch("muon_vz", &muon_vz);
   
-  tree->Branch("muon_isReco", &muon_isReco);
-  tree->Branch("muon_isTightReco", &muon_isTightReco);
+  tree->Branch("muon_isGlboal", &muon_isGlobal);
+  tree->Branch("muon_isTracker", &muon_isTracker);
   tree->Branch("muon_isRPC", &muon_isRPC);
-  tree->Branch("muon_isTightRPC", &muon_isTightRPC);
   tree->Branch("muon_isGEM", &muon_isGEM);
-  tree->Branch("muon_isTightGEM", &muon_isTightGEM);
-  
+  tree->Branch("muon_isTight", &muon_isTight);
+
   tree->Branch("muon_isRecoZ", &muon_isRecoZ);
-  tree->Branch("muon_isTightRecoZ", &muon_isTightRecoZ);
+  tree->Branch("muon_isGlobalZ", &muon_isGlobalZ);
+  tree->Branch("muon_isTightGlobalZ", &muon_isTightGlobalZ);
+  tree->Branch("muon_isTrackerZ", &muon_isTrackerZ);
   tree->Branch("muon_isRPCZ", &muon_isRPCZ);
+  tree->Branch("muon_isTunedRPCZ", &muon_isTunedRPCZ);
   tree->Branch("muon_isTightRPCZ", &muon_isTightRPCZ);
   tree->Branch("muon_isGEMZ", &muon_isGEMZ);
+  tree->Branch("muon_isTunedGEMZ", &muon_isTunedGEMZ);
   tree->Branch("muon_isTightGEMZ", &muon_isTightGEMZ);
 
   // Generator level muon branches
@@ -154,6 +169,7 @@ void AnalysisMC::beginJob() {
   tree->Branch("genMuon_vz", &genMuon_vz);
   tree->Branch("genMuon_isGenZ", &genMuon_isGenZ);
 
+  tree->Branch("muonMCMatchPass", &muonMCMatchPass);
   tree->SetCacheSize(10000000);
   tree->SetMaxVirtualSize(1000000);
 }
@@ -163,7 +179,6 @@ void AnalysisMC::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   edm::Handle<reco::GenParticleCollection> genParticles;
   edm::Handle<GenEventInfoProduct> genInfo;
   edm::Handle<edm::Association<reco::GenParticleCollection>> muonMCMatch;
-  
 
   iEvent.getByToken(muonToken_, muons);
   iEvent.getByToken(genParticleToken_, genParticles);
@@ -177,86 +192,81 @@ void AnalysisMC::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   muon_phi.clear();
   muon_iso.clear();
   muon_vz.clear();
-  muon_isReco.clear();
-  muon_isTightReco.clear();
+  muon_isGlobal.clear();
+  muon_isTracker.clear();
   muon_isRPC.clear();
-  muon_isTightRPC.clear();
   muon_isGEM.clear();
-  muon_isTightGEM.clear();
+  muon_isTight.clear();
   muon_isRecoZ.clear();
-  muon_isTightRecoZ.clear();
+  muon_isGlobalZ.clear();
+  muon_isTightGlobalZ.clear();
+  muon_isTrackerZ.clear();
   muon_isRPCZ.clear();
+  muon_isTunedRPCZ.clear();
   muon_isTightRPCZ.clear();
   muon_isGEMZ.clear();
+  muon_isTunedGEMZ.clear();
   muon_isTightGEMZ.clear();
-
   genMuon_charge.clear();
   genMuon_pt.clear();
   genMuon_eta.clear();
   genMuon_phi.clear();
   genMuon_vz.clear();
   genMuon_isGenZ.clear();
+  muonMCMatchPass.clear();
 
   // Initialize Z boson variables
   std::fill(zMass.begin(), zMass.end(), -1.0);
   std::fill(zDvz.begin(), zDvz.end(), -1.0);
 
 
-  nMatching = 0;
-
-  // Reconstructed muon selection
+  // Reconstructed muon information
 for (const auto& muon : *muons) {
+    //
+    muon_isGlobal.push_back(muon.isGlobalMuon());
+    muon_isTracker.push_back(muon.isTrackerMuon());
+    muon_isRPC.push_back(muon.isRPCMuon());
+    muon_isGEM.push_back(muon.isGEMMuon());
+
+    bool isTight = muon.passed(reco::Muon::PFIsoTight) && muon.passed(reco::Muon::CutBasedIdTight);
+    muon_isTight.push_back(isTight);
+
     // Get the reference to the current muon in the collection
     edm::Ref<reco::MuonCollection> muonRef(muons, &muon - &(*muons)[0]);
-
     // Check if there is a match in the muonMCMatch association
     if (muonMCMatch.isValid()) {
-        // Get the index of the genParticle matched with this muon
-        const edm::Ref<reco::GenParticleCollection> genMatch = (*muonMCMatch)[muonRef];
-
-        if (genMatch.isAvailable()) {
-            ++nMatching;
-        }
+      // Get the index of the genParticle matched with this muon
+      const edm::Ref<reco::GenParticleCollection> genMatch = (*muonMCMatch)[muonRef];
+      muonMCMatchPass.push_back(genMatch.isAvailable());
     }
 
-    if (muon.pt() > 24 && fabs(muon.eta()) < 2.4) {      
-      double iso = (muon.pfIsolationR04().sumChargedHadronPt +
-                   std::max(0.0, muon.pfIsolationR04().sumNeutralHadronEt +
-                           muon.pfIsolationR04().sumPhotonEt -
-                           0.5 * muon.pfIsolationR04().sumPUPt)) / muon.pt();
+    double iso = (muon.pfIsolationR04().sumChargedHadronPt +
+                  std::max(0.0, muon.pfIsolationR04().sumNeutralHadronEt +
+                  muon.pfIsolationR04().sumPhotonEt -
+                  0.5 * muon.pfIsolationR04().sumPUPt)) / muon.pt();
 
-      bool isTight = muon.passed(reco::Muon::PFIsoTight) && muon.passed(reco::Muon::CutBasedIdTight);
-      
-      muon_charge.push_back(muon.charge());
-      muon_pt.push_back(muon.pt());
-      muon_eta.push_back(muon.eta());
-      muon_phi.push_back(muon.phi());
-      muon_iso.push_back(iso);
-      muon_vz.push_back(muon.vz());
+    muon_charge.push_back(muon.charge());
+    muon_pt.push_back(muon.pt());
+    muon_eta.push_back(muon.eta());
+    muon_phi.push_back(muon.phi());
+    muon_iso.push_back(iso);
+    muon_vz.push_back(muon.vz());
 
-      muon_isReco.push_back(true);
-      muon_isTightReco.push_back(isTight);
-      muon_isRPC.push_back(muon.isRPCMuon());
-      muon_isTightRPC.push_back(muon.isRPCMuon() && isTight);
-      muon_isGEM.push_back(muon.isGEMMuon());
-      muon_isTightGEM.push_back(muon.isGEMMuon() && isTight);
-
-      muon_isRecoZ.push_back(false);
-      muon_isTightRecoZ.push_back(false);
-      muon_isRPCZ.push_back(false);
-      muon_isTightRPCZ.push_back(false);
-      muon_isGEMZ.push_back(false);
-      muon_isTightGEMZ.push_back(false);
-    }
+    muon_isRecoZ.push_back(false);
+    muon_isGlobalZ.push_back(false);
+    muon_isTightGlobalZ.push_back(false);
+    muon_isTrackerZ.push_back(false);
+    muon_isRPCZ.push_back(false);
+    muon_isTunedRPCZ.push_back(false);
+    muon_isTightRPCZ.push_back(false);
+    muon_isGEMZ.push_back(false);
+    muon_isTunedGEMZ.push_back(false);
+    muon_isTightGEMZ.push_back(false);
   }
 
-  if (nMatching < 2) {
-    return;  // Skip this event if matching failed (less than 2 valid reco muons)
-  }
-
-  // Generator level muon selection
+  // Generator level muon information
   for (const auto& genParticle : *genParticles) {
-    if (abs(genParticle.pdgId()) == 13 && genParticle.pt() > 24 && fabs(genParticle.eta()) < 2.4) {
+    if (abs(genParticle.pdgId()) == 13) {
       genMuon_charge.push_back(genParticle.charge());
       genMuon_pt.push_back(genParticle.pt());
       genMuon_eta.push_back(genParticle.eta());
@@ -265,7 +275,6 @@ for (const auto& muon : *muons) {
       genMuon_isGenZ.push_back(false);
     }
   }
-
   // Generator level Z reconstruction
   auto reconstructGenZBoson = [this]() {
     double bestMass = std::numeric_limits<double>::max();
@@ -274,8 +283,10 @@ for (const auto& muon : *muons) {
     int bestMuon2 = -1;
 
     for (size_t i = 0; i < genMuon_pt.size(); ++i) {
+      if (genMuon_pt[i] > 24 && fabs(genMuon_eta[i])) continue;
       for (size_t j = i + 1; j < genMuon_pt.size(); ++j) {
         if (genMuon_charge[i] + genMuon_charge[j] != 0) continue;
+        if (genMuon_pt[j] > 24 && fabs(genMuon_eta[j])) continue;
 
         double dvz = fabs(genMuon_vz[i] - genMuon_vz[j]);
         math::PtEtaPhiMLorentzVector p4_1(genMuon_pt[i], genMuon_eta[i], genMuon_phi[i], 0.10566);
@@ -293,8 +304,8 @@ for (const auto& muon : *muons) {
 
     if (bestMuon1 != -1) {
       genMuon_isGenZ[bestMuon1] = genMuon_isGenZ[bestMuon2] = true;
-      zMass[6] = bestMass;
-      zDvz[6] = minDvz;
+      zMass[10] = bestMass;
+      zDvz[10] = minDvz;
     }
   };
 
@@ -325,24 +336,39 @@ for (const auto& muon : *muons) {
       }
     }
 
+
+
+
     if (bestMuon1 != -1) {
       switch(categoryIndex) {
         case 0: 
           muon_isRecoZ[bestMuon1] = muon_isRecoZ[bestMuon2] = true; 
           break;
         case 1: 
-          muon_isTightRecoZ[bestMuon1] = muon_isTightRecoZ[bestMuon2] = true; 
+          muon_isGlobalZ[bestMuon1] = muon_isGlobalZ[bestMuon2] = true; 
           break;
         case 2: 
-          muon_isRPCZ[bestMuon1] = muon_isRPCZ[bestMuon2] = true; 
+          muon_isTightGlobalZ[bestMuon1] = muon_isTightGlobalZ[bestMuon2] = true; 
           break;
         case 3: 
-          muon_isTightRPCZ[bestMuon1] = muon_isTightRPCZ[bestMuon2] = true; 
+          muon_isTrackerZ[bestMuon1] = muon_isTrackerZ[bestMuon2] = true; 
           break;
         case 4: 
-          muon_isGEMZ[bestMuon1] = muon_isGEMZ[bestMuon2] = true; 
+          muon_isRPCZ[bestMuon1] = muon_isRPCZ[bestMuon2] = true; 
           break;
         case 5: 
+          muon_isTunedRPCZ[bestMuon1] = muon_isTunedRPCZ[bestMuon2] = true; 
+          break;
+        case 6: 
+          muon_isTightRPCZ[bestMuon1] = muon_isTightRPCZ[bestMuon2] = true; 
+          break;
+        case 7: 
+          muon_isGEMZ[bestMuon1] = muon_isGEMZ[bestMuon2] = true; 
+          break;
+        case 8: 
+          muon_isTunedGEMZ[bestMuon1] = muon_isTunedGEMZ[bestMuon2] = true; 
+          break;
+        case 9: 
           muon_isTightGEMZ[bestMuon1] = muon_isTightGEMZ[bestMuon2] = true; 
           break;
       }
@@ -354,12 +380,16 @@ for (const auto& muon : *muons) {
 
   // Apply Z boson reconstruction for both gen and reco categories
   reconstructGenZBoson();
-  reconstructZBoson(0, [this](size_t i) { return muon_isReco[i]; });
-  reconstructZBoson(1, [this](size_t i) { return muon_isTightReco[i]; });
-  reconstructZBoson(2, [this](size_t i) { return muon_isRPC[i]; });
-  reconstructZBoson(3, [this](size_t i) { return muon_isTightRPC[i]; });
-  reconstructZBoson(4, [this](size_t i) { return muon_isGEM[i]; });
-  reconstructZBoson(5, [this](size_t i) { return muon_isTightGEM[i]; });
+  reconstructZBoson(0, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])); });
+  reconstructZBoson(1, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isGlobal[i]; });
+  reconstructZBoson(2, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isGlobal[i] && muon_isTight[i]; });
+  reconstructZBoson(3, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isTracker[i]; });
+  reconstructZBoson(4, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isRPC[i]; });
+  reconstructZBoson(5, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isRPC[i] && muon_isGlobal[i]; });
+  reconstructZBoson(6, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isRPC[i] && muon_isTight[i]; });
+  reconstructZBoson(7, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isGEM[i]; });
+  reconstructZBoson(8, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isGEM[i] && muon_isGlobal[i]; });
+  reconstructZBoson(9, [this](size_t i) { return (muonMCMatchPass[i] && muon_pt[i] > 24 && fabs(muon_eta[i])) && muon_isGEM[i] && muon_isTight[i]; });
 
   // Event data
   eventNumber = iEvent.id().event();
